4b60373f7af1acb8d8b1b1af3e6237d977dca78e
yakubszatkowski <yakub.szatkowski@gmail.com>
2022-12-02 22:50:57 +0100

    add day 17

---
+++ Day 17 - The Quiz Game/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, question_text, question_answer):
+        self.text = question_text
+        self.answer = question_answer
+
+# my_question = Question('here is text', 'here is answer')
+# print(my_question.text)
+# print(my_question.answer)
