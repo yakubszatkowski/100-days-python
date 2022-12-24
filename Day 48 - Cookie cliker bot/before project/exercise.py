1a0caa2bd7eaea31a9e66757a9b8d012abdc321d
yakubszatkowski <yakub.szatkowski@gmail.com>
2022-12-24 16:14:06 +0100

    day49

--- Day 48 - Cookie cliker bot/before project/exercise.py
+++ Day 48 - Cookie cliker bot/before project/exercise.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+import os
 
 URL = 'https://www.python.org/'
 chrome_driver_path = os.environ.get('D48_chrome_driver_path')
@@ -10,7 +10,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service(executable_path=chrome_driver_path, log_path="NUL"))
 driver.get(URL)
-driver.maximize_window()
+# driver.maximize_window()
 time.sleep(0.25)
 
 # upcoming_events = driver.find_element(by=By.CSS_SELECTOR, value='.event-widget .shrubbery .menu')
@@ -22,14 +22,16 @@ time.sleep(0.25)
 #     event_dict[num] = {'time': events_dates[num], 'name': events_names[num]}
 # print(event_dict)
 
-events_dates = driver.find_elements(by=By.CSS_SELECTOR, value='.event-widget time')
-events_names = driver.find_elements(by=By.CSS_SELECTOR, value='.event-widget .menu a')
-events_dict = {}
-for num in range(len(events_dates)):
-    events_dict[num] = {
-        'time': events_dates[num].text,
-        'name': events_names[num].text,
-    }
-
-print(events_dict)
-driver.close()
+# events_dates = driver.find_elements(by=By.CSS_SELECTOR, value='.event-widget time')
+# events_names = driver.find_elements(by=By.CSS_SELECTOR, value='.event-widget .menu a')
+# events_dict = {}
+# for num in range(len(events_dates)):
+#     events_dict[num] = {
+#         'time': events_dates[num].text,
+#         'name': events_names[num].text,
+#     }
+#
+# print(events_dict)
+# driver.close()
+logo = driver.find_element(By.CSS_SELECTOR, '.subnav menu')
+print(logo)
---
+++ Day 49 - LinkedIn automation/Start.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+import selenium.common.exceptions
+import time
+import os
+
+linkedin_mail = os.environ.get('D49_linkedin_mail')
+linkedin_pass = os.environ.get('D49_linkedin_pass')
+
+URL = 'https://www.linkedin.com/jobs/search/?currentJobId=3403355181&f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom'
+chrome_driver_path = os.environ.get('D48_chrome_driver_path')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=Service(executable_path=chrome_driver_path, log_path="NUL"))
+driver.get(URL)
+# driver.maximize_window()
+
+time.sleep(1)
+log_in = driver.find_element(By.CSS_SELECTOR, '.base-serp-page__header .nav .nav__button-secondary')
+log_in.click()
+time.sleep(1)
+
+e_mail_box = driver.find_element(By.XPATH, '//*[@id="username"]')
+password_box = driver.find_element(By.XPATH, '//*[@id="password"]')
+submit_button = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
+
+e_mail_box.send_keys(linkedin_mail)
+password_box.send_keys(linkedin_pass)
+submit_button.click()
+time.sleep(20)  # verification bs - pick picture that shows this and that
+
+try:  # verification bs - sometimes element of closing changes ID
+    dumb_button = driver.find_element(By.ID, 'ember115')
+except NoSuchElementException:
+    try:
+        dumb_button = driver.find_element(By.ID, 'ember114')
+    except NoSuchElementException:
+        dumb_button = driver.find_element(By.ID, 'ember116')
+
+dumb_button.click()
+
+jobs_list = driver.find_elements(By.CLASS_NAME, 'jobs-search-results__list-item')
+for job in jobs_list:
+    driver.execute_script("arguments[0].scrollIntoView();", job)  # scrolls until the object is visible
+    job.click()
+    time.sleep(2)
+    save_button = driver.find_element(By.CSS_SELECTOR, '.application-outlet .scaffold-layout__detail '
+                                                       '.jobs-unified-top-card .jobs-save-button')
+
+    follow_button = driver.find_element(By.CSS_SELECTOR, '.application-outlet .scaffold-layout__detail '
+                                                         '.jobs-company .follow')
+    try:
+        # sometimes button is not visible because of random popup stuff,
+        # so I am scrolling slightly below Y coordinate of button
+        save_button.click()
+    except ElementClickInterceptedException:
+        y_cord_save_button = int(save_button.location['y'])
+        driver.execute_script(f"window.scrollTo(0, {y_cord_save_button + 450})")
+        save_button.click()
+    time.sleep(1)
+    driver.execute_script("arguments[0].scrollIntoView();", follow_button)
+    follow_button.click()
+    time.sleep(1)
