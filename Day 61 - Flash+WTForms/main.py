48dbe8bd9ca768d70d8148baa967294a51994720
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-01-24 05:29:42 +0100

    d61

---
+++ Day 61 - Flash+WTForms/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
+import os
+
+# Flask instance
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('D61_secret_key')
+
+
+# Flask form
+class NameForm(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])  # or label = "E-mail", same with password
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    login = SubmitField('Submit')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    email = None
+    password = None
+    form = NameForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        if email == 'admin@email.com' and password == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html',
+                           email=email,
+                           password=password,
+                           form=form,)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
