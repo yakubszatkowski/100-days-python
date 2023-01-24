a99dfb3e157275a27a67b6d0798fff7d756352cc
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-01-24 20:16:59 +0100

    d61cd

--- Day 61 - Flash+WTForms/main.py
+++ Day 61 - Flash+WTForms/main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap5
 import os
 
 # Flask instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('D61_secret_key')
+bootstrap = Bootstrap5(app)
 
 
 # Flask form
@@ -36,7 +38,7 @@ def login():
     return render_template('login.html',
                            email=email,
                            password=password,
-                           form=form,)
+                           form=form, )
 
 
 if __name__ == '__main__':
