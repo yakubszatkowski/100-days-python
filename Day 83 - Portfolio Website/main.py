1a6c73c97b998f9a41d76a537a683f159cf08c23
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-09-18 16:28:00 +0200

    feat: replace button with hyperlink

--- Day 83 - Portfolio Website/main.py
+++ Day 83 - Portfolio Website/main.py
@@ -11,5 +11,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/main/<language>')
+def main_page(language):
+    if language == 'polish':
+        return render_template('main.html')
+    if language == 'english':
+        return render_template('main.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
