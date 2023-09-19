9454684e7e19d785bf1ebd2ef7474135b4fd4b4a
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-09-19 17:43:11 +0200

    feat: new post method for creating user

--- Side projects/basic API/test API simplified.py
+++ Side projects/basic API/test API simplified.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route("/get-user/<user_id>")
+@app.route('/get-user/<user_id>')
 def get_user(user_id):
     user_data = {
         'user_id': user_id,
@@ -12,10 +12,30 @@ def get_user(user_id):
     }
 
     extra = request.args.get('extra')
+    extra2 = request.args.get('extra2')
     if extra:
         user_data['extra'] = extra
+    if extra2:
+        user_data['extra2'] = extra2
 
     return jsonify(user_data)
+
+
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    # if request.method == 'post':  # this is used when having multiple methods
+    data = request.get_json()
+    return jsonify(data)
+
+
+@app.route('/adv-create-user', methods=['POST'])
+def advanced_create_user():
+    data = {
+        'user_id': request.args.get('user_id'),
+        'name': request.args.get('name', 'None'),
+        'email': request.args.get('email', 'None')
+    }
+    return jsonify(data)
 
 
 if __name__ == '__main__':
