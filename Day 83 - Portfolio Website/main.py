d5b50fce0a1b5cb934776ac6a86975936330fb6e
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-09-27 21:51:33 +0200

    feat: introduce initial database

--- Day 83 - Portfolio Website/main.py
+++ Day 83 - Portfolio Website/main.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Technology(db.Model):
+    __tablename__ = 'technologies'
+    id = db.Column(db.Integer, primary_key=True)
+    technology_name = db.Column(db.String(50), nullable=False)
+    subtechnologies = db.relationship('SubTechnology', backref='technology')  # what does backref does?
+
+
+class SubTechnology(db.Model):
+    __tablename__ = 'subtechnologies'
+    id = db.Column(db.Integer, primary_key=True)
+    technology_name = db.Column(db.String, db.ForeignKey('technologies.technology_name'))
+    subtechnology_name = db.Column(db.String(50), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/index')
