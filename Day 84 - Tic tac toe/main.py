edbe8c1b8a61aa965ce298cc5592fb418cef81c0
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-10-17 19:34:38 +0200

    feat: function that checks if O or X won

--- Day 84 - Tic tac toe/main.py
+++ Day 84 - Tic tac toe/main.py
@@ -8,16 +8,29 @@ import itertools
 LabelBase.register(name='Montserrat',
                    fn_regular='styling/Montserrat-Light.ttf')
 
+win_conditions = {
+    1: (0, 1, 2),
+    2: (3, 4 ,5),
+    3: (6, 7, 8),
+    4: (0, 3, 6),
+    5: (1, 4, 7),
+    6: (2, 5, 8),
+    7: (2, 4, 6),
+    8: (0, 4, 8),
+}
+
 
 class MainWindow(Screen):
     pass
 
 
 class GameWindow(Screen):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.game_board = None
         self.alternate_xo = None
+
 
     def on_enter(self):
         self.alternate_xo = itertools.cycle(['O', 'X'])  # consider different colors?
@@ -28,16 +41,38 @@ class GameWindow(Screen):
             button = Button(text="", font_size=80, on_press=self.clicked)
             self.game_board.add_widget(button)
 
+
+    def check_if_win(self):  # check the logic of win conditions
+        empty_board = self.game_board.children[::-1]
+        board_state = [element.text for element in empty_board]
+
+        print(f'{board_state[:3]}\n{board_state[3:6]}\n{board_state[6:8]}\n')  # testing
+
+        for win_condition in win_conditions.values():
+            x_count = 0
+            o_count = 0
+            for position in win_condition:
+                if board_state[position] == 'X':
+                    x_count += True
+                    if x_count == 3:
+                        print('X won!')
+                if board_state[position] == 'O':
+                    o_count += True
+                    if o_count == 3:
+                        print('O won!')
+
+
     def on_leave(self):
         for element in self.game_board.children[::-1]:  # this iterates backwards
             element.text = ''
-            # print(element.text)
+
 
     def clicked(self, button):
         if button.text:
             pass
         else:
             button.text = next(self.alternate_xo)
+            self.check_if_win()
 
 
 class WindowManager(ScreenManager):
