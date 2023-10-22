3f227691b91005bde956ce1c13abad49717d638b
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-10-22 08:38:32 +0200

    perf: initializing code structure for better performance

--- Day 84 - Tic tac toe/main.py
+++ Day 84 - Tic tac toe/main.py
@@ -26,75 +26,75 @@ class MainWindow(Screen):
     pass
 
 
-class Game(Screen):
-    score_x = NumericProperty()
-    score_o = NumericProperty()
-    winner = StringProperty()
-
-    def on_enter(self):
-        self.alternate_xo = itertools.cycle(['O', 'X'])  # consider different colors?
-        self.game_board = self.ids.game_board
-        self.game_board.clear_widgets()
-        self.score_x = 0
-        self.score_o = 0
-
-        for n in range(9):
-            button = Button(text="", font_size=80, on_press=self.clicked)
-            self.game_board.add_widget(button)
-
-
-    def clicked(self, button):
-        if button.text:
-            pass
-        else:
-            button.text = next(self.alternate_xo)
-            self.check_if_win()
-
-
-    def check_if_win(self):
-
-        def win(winner):
-            for button in self.game_board.children:
-                button.disabled=True
-            self.winner = f'{winner} won!'
-            Clock.schedule_once(self.on_leave, 1)
-
-        empty_board = self.game_board.children[::-1]
-        board_state = [element.text for element in empty_board]
-
-        for win_condition in win_conditions:
-            x_count = sum(True for position in win_condition if board_state[position] == 'X')
-            o_count = sum(True for position in win_condition if board_state[position] == 'O')
-
-            if x_count == 3:
-                self.score_x += 1
-                win('X')
-            elif o_count == 3:
-                self.score_o += 1
-                win('O')
-
-
-    def on_leave(self, dt=None):
-        self.winner = ""
-        for button in self.game_board.children:
-            button.disabled = False
-        for element in self.game_board.children[::-1]:  # this is supposed to iterate backwards
-            element.text = ''
-
-
-class PvP(Game):
-    pass
-
-
 class VsComputerOption(Screen):
     pass
 
 
-class VsComputerEasy(Game):
-    pass
+class Game(Screen):
+
+    score_1 = NumericProperty()
+    score_2 = NumericProperty()
+    winner = StringProperty()
+
+    def on_enter(self):
+        self.game_board = self.ids.game_board
+        self.game_board.clear_widgets()
+        self.score_1 = 0
+        self.score_2 = 0
+        self.turn = 'player_1'
+
+        for n in range(9):
+            self.button = Button(text="", font_size=80, on_press=self.player_moves)
+            self.game_board.add_widget(self.button)
+
+    def on_leave(self, dt=None):
+        self.winner = ''
+        for button in self.game_board.children:
+            button.disabled = False
+        for element in self.game_board.children[::-1]:
+            element.text = ''
+
+    def is_win(self, figure):
+        board_objects = self.game_board.children[::-1]
+        board = [element.text for element in board_objects]
+        for win_condition in win_conditions:
+            count = sum(True for position in win_condition if board[position] == figure)
+            if count == 3:
+                print(f'{figure} win')
+                return True
+        return False
+
+
+class PvP(Game):
+
+    def player1_move(self, button):
+        if button.text:
+            pass
+        else:
+            button.text = 'O'
+            self.is_win('O')
+
+    def player2_move(self, button):
+        if button.text:
+            pass
+        else:
+            button.text = 'X'
+            self.is_win('X')
+
+    def player_moves(self, button):
+        if self.turn == 'player_1':
+            self.player1_move(button)
+            self.turn = 'player_2'
+        elif self.turn == 'player_2':
+            self.player2_move(button)
+            self.turn = 'player_1'
 
 
 class VsComputerHard(Game):
+    pass
+
+
+class VsComputerEasy(Game):
     pass
 
 
