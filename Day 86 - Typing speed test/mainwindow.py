8a206dcd1fc0732c158600412bda3f6fdfb6dc61
yakubszatkowski <yakub.szatkowski@gmail.com>
2023-12-10 18:49:57 +0100

    feat: signal when entering test layout, pick a random text and put it in text_label

--- Day 86 - Typing speed test/mainwindow.py
+++ Day 86 - Typing speed test/mainwindow.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import QMainWindow, QWidget, QStackedLayout
 from PySide6.QtCore import Qt
 from ui_menuwidget import Ui_menu_widget
 from ui_testwidget import Ui_test_widget
+from random import choice
+from jokes_list import it_jokes
 
 class MenuWidget(QWidget, Ui_menu_widget):
     def __init__(self, parent):
@@ -9,9 +11,11 @@ class MenuWidget(QWidget, Ui_menu_widget):
         self.setupUi(self)
         self.main_window = parent
         self.setAttribute(Qt.WA_StyledBackground, True)
-        self.start_button.clicked.connect(self.start_test)
 
-    def start_test(self):
+        self.start_button.clicked.connect(self.switch_to_test)
+        self.start_button.setShortcut('Return')
+
+    def switch_to_test(self):
         self.main_window.switch_window(1)
 
 class TestWidget(QWidget, Ui_test_widget):
@@ -20,10 +24,17 @@ class TestWidget(QWidget, Ui_test_widget):
         self.setupUi(self)
         self.main_window = parent
         self.setAttribute(Qt.WA_StyledBackground, True)
-        self.return_button.clicked.connect(self.return_to_menu)
 
-    def return_to_menu(self):
+        self.return_button.clicked.connect(self.switch_to_menu)
+        self.again_button.clicked.connect(self.choose_text)
+
+    def switch_to_menu(self):
         self.main_window.switch_window(0)
+
+    def choose_text(self):
+        random_text = choice(it_jokes)
+        self.text_label.setText(random_text)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -41,12 +52,19 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(self.stacked_layout)
         self.setCentralWidget(central_widget)
 
+        self.stacked_layout.currentChanged.connect(self.switch_to_test)
+
     def switch_window(self, index):
         self.stacked_layout.setCurrentIndex(index)
 
+    def switch_to_test(self, index):
+        if index == 1:
+            self.test.choose_text()
+
+
 #TODO
-# create list of random texts
-# pick random text and put it in text_label when entering test
+# create list of random texts - done
+# pick random text and put it in text_label when entering test, signal when entering the layout - done
 # when entering test enable countdown mechanism - 5 seconds
 # after countdown enable input_textedit, start the timer in results_label in s:ms format
 # dynamically highlight letters in text_label that match the letters from input_textedit
