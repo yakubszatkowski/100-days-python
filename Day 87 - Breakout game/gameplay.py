ae0429e2c88fb99d216987b9f9ebf410a23c7365
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-01-09 13:10:40 +0100

    refactor: draw sprites, initial movement

--- Day 87 - Breakout game/gameplay.py
+++ Day 87 - Breakout game/gameplay.py
@@ -3,10 +3,17 @@ import math
 
 class GamePlay:
     def __init__(self, game):
-        self.right_border, self.left_border, self.board_objects, self.player_block, self.ball = None, None, None, None, None
+        self.ball, self.player_block = None, None
+        self.right_border, self.left_border = None, None
         self.gameplay = False
         self.game = game
         self.clock = pygame.time.Clock()
+
+        self.player_group = pygame.sprite.Group()
+        self.ball_group = pygame.sprite.Group()
+        self.block_group = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group()
+        # self.all_sprites.add(self.player_group, self.ball_group, self.block_group)
 
     def blit_screen(self):
         self.game.window.blit(self.game.window, (0, 0))
@@ -25,15 +32,22 @@ class GamePlay:
     def init_board(self):
         self.left_border = pygame.Rect(27, 0, 10, self.game.HEIGHT)
         self.right_border = pygame.Rect(self.game.WIDTH - 30, 0, 10, self.game.HEIGHT)
-        self.player_block = PlayerBlock(self.game.WIDTH / 2 - 50, 700, 100, 20)
-        self.ball = Ball(self.game.WIDTH / 2 - 5, self.game.HEIGHT / 2, 10, 10)
+        self.player_block = PlayerBlock(self.game.WIDTH / 2 - 50, 700)
+        self.ball = Ball(self.game.WIDTH / 2 - 5, self.game.HEIGHT / 2)
+
+        self.player_group.add(self.player_block)
+        self.ball_group.add(self.ball)
+        # self.block_group.add()
+        self.all_sprites.add(self.player_group, self.ball_group, self.block_group)
+
 
     def draw_board(self):
         self.game.window.fill(self.game.BLACK)
         pygame.draw.rect(self.game.window, self.game.WHITE, self.left_border)
         pygame.draw.rect(self.game.window, self.game.WHITE, self.right_border)
-        pygame.draw.rect(self.game.window, self.game.WHITE, self.player_block)
-        pygame.draw.circle(self.game.window, self.game.WHITE, self.ball.center, 5)
+        self.all_sprites.update()
+        self.all_sprites.draw(self.game.window)
+
 
     def check_input(self):
         keys = pygame.key.get_pressed()
--- Day 87 - Breakout game/objects.py
+++ Day 87 - Breakout game/objects.py
@@ -1,19 +1,38 @@
 import pygame
 
 class PlayerBlock(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y):
         super().__init__()
-        self.width = width
-        self.image = pygame.Surface((width, height))
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.width, self.height = 100, 15
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill('white')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.VELOCITY = 5
 
+    def update(self, *args, **kwargs):
+        self.move()
+        # self.check_collision()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.VELOCITY
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.VELOCITY
 
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((width, height))
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.center = (x, y)
+        self.width, self.height = 10, 10
+        self.image = pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
         self.BALL_SPEED = 5
 
+        pygame.draw.circle(self.image, 'white', (self.width // 2, self.height // 2), 5)
+
+    def update(self):
+        self.rect.y += self.BALL_SPEED
+        self.rect.x += self.BALL_SPEED
