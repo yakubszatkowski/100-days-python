a047f50e1163bfd5676fe75cc9cc34bdd2a66a91
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-01-20 23:12:36 +0100

    feat: blocks, collission between blocks and ball, bounce mechanic

--- Day 87 - Breakout game/gameplay.py
+++ Day 87 - Breakout game/gameplay.py
@@ -12,7 +12,6 @@ class GamePlay:
         self.block_group = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
 
-
     def blit_screen(self):
         self.game.window.blit(self.game.window, (0, 0))
         pygame.display.update()
@@ -31,9 +30,17 @@ class GamePlay:
     def init_board(self):
         self.left_border = pygame.Rect(27, 0, 10, self.game.HEIGHT)
         self.right_border = pygame.Rect(self.game.WIDTH - 30, 0, 10, self.game.HEIGHT)
-        self.player_block = PlayerBlock(430, 700)  # self.game.WIDTH / 2 - 50
-        self.ball = Ball(self.game.WIDTH / 2 - 5, self.game.HEIGHT / 2, self.game, self, self.player_block)
-        self.all_sprites.add(self.player_block, self.ball, self.block_group)
+        self.player_block = PlayerBlock(self.game.WIDTH / 2 - 50 + 36, 700)
+        start_x, start_y = self.left_border.x + 37, 30
+        for i in range(90):
+            block = Block(start_x, start_y)
+            self.block_group.add(block)
+            start_x += block.width + 5
+            if len(self.block_group) % 9 == 0:
+                start_x = self.left_border.x + 37  # 37 originally
+                start_y += 30
+        self.ball = Ball(self.game.WIDTH / 2 - 5, self.game.HEIGHT / 2, self.game, self)
+        self.all_sprites.add(self.player_block, self.block_group, self.ball)
 
     def draw_board(self):
         self.game.window.fill(self.game.BLACK)
--- Day 87 - Breakout game/objects.py
+++ Day 87 - Breakout game/objects.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 class PlayerBlock(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -22,40 +23,57 @@ class PlayerBlock(pygame.sprite.Sprite):
             self.rect.x += self.VELOCITY
 
 
+class Block(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.color_list = [
+            (77, 128, 77),
+            (128, 64, 64),
+            (77, 77, 128),
+            (204, 173, 179),
+            (204, 204, 102),
+            (89, 0, 89),
+            (204, 133, 102),
+            (77, 128, 128),
+            (204, 46, 0)
+        ]
+        self.width, self.height = 50, 20
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(random.choice(self.color_list))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, x, y, game, gameplay, player):
+    def __init__(self, x, y, game, gameplay):
         super().__init__()
         self.game, self.gameplay = game, gameplay
-        self.player = player
+        self.player = self.gameplay.player_block
+        self.blocks = self.gameplay.block_group
         self.width, self.height = 10, 10
         self.image = pygame.Surface((self.width, self.height))
         self.rect = self.image.get_rect()
         pygame.draw.circle(self.image, 'white', (self.width // 2, self.height // 2), 5)
         self.rect.topleft = (x, y)
-
-        self.direction = 120
-        self.x_dir, self.y_dir = 0, 0
+        self.direction = 180
         self.velocity = 5
-
+        self.collision_tolerance = 10
 
     def update(self):
-
         def bounce(angle):
             self.direction = 360 - self.direction
             self.direction += angle
             if self.direction >= 360:
                 self.direction -= 360
-            if 280 > self.direction > 260:
-                self.direction += 25
-            elif 100 > self.direction > 80:
-                self.direction -= 25
-
-            print(f'current direction: {self.direction}')
+            if 285 > self.direction > 255:
+                self.direction += 31
+            elif 105 > self.direction > 75:
+                self.direction -= 31
             return self.direction
 
         def player_bounce(block):
             diff = int((block.rect.x + block.rect.width / 2) - (self.rect.x + self.rect.width / 2))
-            if block.rect.top <= self.rect.bottom < block.rect.top + 6 and y_dir > 0:
+            if block.rect.top <= self.rect.bottom < block.rect.top + self.collision_tolerance and y_dir > 0:
                 bounce_angle = 180 - diff
                 bounce_angle = bounce(bounce_angle)
                 if 270 > bounce_angle > 90:
@@ -66,9 +84,21 @@ class Ball(pygame.sprite.Sprite):
         self.rect.x += self.velocity * x_dir
         self.rect.y += self.velocity * y_dir
 
+        for block in self.blocks:
+            if self.rect.colliderect(block):
+                if abs(block.rect.top - self.rect.bottom) < self.collision_tolerance and y_dir > 0:
+                    bounce(180)
+                if abs(block.rect.bottom - self.rect.top) < self.collision_tolerance and y_dir < 0:
+                    bounce(180)
+                if abs(block.rect.right - self.rect.left) < self.collision_tolerance and x_dir < 0:
+                    bounce(0)
+                if abs(block.rect.left - self.rect.right) < self.collision_tolerance and x_dir > 0:
+                    bounce(0)
+                block.kill()
+
         if self.rect.colliderect(self.player):
             player_bounce(self.player)
-        elif self.rect.y <= 0 + 350:  # change to +10 later on
+        elif self.rect.y <= 0 + 10:
             self.rect.y += 10
             bounce(180)
         elif self.rect.y >= self.game.HEIGHT - 10:  # losing condition
