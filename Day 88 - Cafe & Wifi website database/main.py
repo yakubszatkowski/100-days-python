26637751d00fdfebd1ab5c058fd6c39fc0b3e09e
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-01-29 00:17:05 +0100

    feat: search button, input bar styling, POST search input

--- Day 88 - Cafe & Wifi website database/main.py
+++ Day 88 - Cafe & Wifi website database/main.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+# from flask_restful
 import os
 
 app = Flask(__name__)
 API_KEY = os.environ.get("Google_API_KEY")
 
 
-@app.route('/index')
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        search_input = request.form['search_input']
+        print(search_input)
     return render_template('index.html', API_KEY=API_KEY)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Day 88 - Cafe & Wifi website database/static/js/place_autocomplete.js
+++ Day 88 - Cafe & Wifi website database/static/js/place_autocomplete.js
@@ -7,4 +7,17 @@ function initAutocomplete() {
             fields: ['place_id', 'geometry', 'name']
         }
     );
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+            document.getElementById('autocomplete').placeholder = 'Enter a place';
+    }
+    else {
+        document.getElementById('details').innerHTML = place.name;
+    }
 }
