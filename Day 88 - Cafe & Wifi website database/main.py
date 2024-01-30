d8732b23d81fe37e91b7f0cd3e7c7bc5fe2bbde2
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-01-30 01:15:09 +0100

    feat: form submit on choosen autocomplete option

--- Day 88 - Cafe & Wifi website database/main.py
+++ Day 88 - Cafe & Wifi website database/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-# from flask_restful
 import os
 
 app = Flask(__name__)
--- Day 88 - Cafe & Wifi website database/static/js/place_autocomplete.js
+++ Day 88 - Cafe & Wifi website database/static/js/place_autocomplete.js
@@ -1,4 +1,5 @@
-let autocomplete
+let autocomplete; 
+
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('autocomplete'),
@@ -11,13 +12,8 @@ function initAutocomplete() {
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
 
+
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
-
-    if (!place.geometry) {
-            document.getElementById('autocomplete').placeholder = 'Enter a place';
-    }
-    else {
-        document.getElementById('details').innerHTML = place.name;
-    }
+    document.getElementById('cafe-form').submit();
 }
