f791f6863637f904576be2d1bf2142cccc7a13e2
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-02-17 22:13:56 +0100

    feat: singular task deletion, checkboxes

--- Day 89 - Todo List/main.py
+++ Day 89 - Todo List/main.py
@@ -8,12 +8,14 @@ task_list = []
 def index():
     if request.method == 'POST':
         input_task = request.form.get('task')
-        if input_task:
+        if input_task:  # add
             task_list.append(input_task)
-            print(task_list)
-        if request.form.get('reset_button'):
+        if request.form.get('delete_task_button'):  # delete
+            task_index = int(request.form.get('task_index')) - 1
+            task_list.pop(task_index)
+        if request.form.get('reset_button'):  # delete all
             task_list.clear()
-            print(task_list)
+
 
     return render_template('index.html', tasks=task_list)
 
@@ -23,4 +25,9 @@ if __name__ == '__main__':
 
 
 #TODO
+    # edit task's text
+    # warning before deliting all tasks
+    # changing tasks order by click and drag
+    # gratulations message when all tasks are finished
+
     # website reference: https://flask.io/PEhyTiWzw08V
