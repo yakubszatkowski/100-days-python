5e1b6bed1a9b2225b5b40d7e71588215a3a50325
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-02-17 18:53:14 +0100

    feat: temporary tasks storage; reset button; hover styling

--- Day 89 - Todo List/main.py
+++ Day 89 - Todo List/main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+task_list = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        input_task = request.form.get('task')
+        if input_task:
+            task_list.append(input_task)
+            print(task_list)
+        if request.form.get('reset_button'):
+            task_list.clear()
+            print(task_list)
+
+    return render_template('index.html', tasks=task_list)
 
 
 if __name__ == '__main__':
