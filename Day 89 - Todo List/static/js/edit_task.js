dead61060e533f2d7cda20c6bcc1e0995611d793
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-02-21 21:04:13 +0100

    refactor: text input edit instead of editable content

--- Day 89 - Todo List/static/js/edit_task.js
+++ Day 89 - Todo List/static/js/edit_task.js
@@ -1,15 +1,34 @@
-var tasks = document.getElementsByClassName('task')
+var tasks = document.getElementsByClassName('task');
+var enterEvent = new KeyboardEvent('keydown', { key: 'Enter', keyCode: 13 });
 
-for(var i = 0; i < tasks.length; i++) {
-    const task = tasks[i]
+const submitInput = (event) => {
+    if (event.key == 'Enter')
+}
 
-    const edit_button = task.querySelector('.edit-task-button')
-    const task_text = task.querySelector('.task-text')
+const createInput = (text, event) => {
+    event.preventDefault();
+    var string_value = text.textContent;
 
-    edit_button.addEventListener("click", function(event) {
-        event.preventDefault()
-        task_text.contentEditable = true
-        task_text.focus()
+    var edit_input = document.createElement('input');
+    edit_input.type = 'text';
+    edit_input.value = string_value
+    edit_input.classList.add('task-text', 'edit-text')
+    edit_input.name = 'edit_task_input'
 
+    text.parentNode.replaceChild(edit_input, text)
+
+    text.addEventListener('keypress', function(event) {
+        submitInput(event)
     })
+}
+
+for (var i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+
+    const edit_button = task.querySelector('.edit-task-button');
+    const task_text = task.querySelector('.task-text');
+
+    edit_button.addEventListener("click", function (event) {
+        createInput(task_text, event);
+    });
 }
