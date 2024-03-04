d418660fdffab4e82e1d983d640bcc869f4ee710
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-03-04 05:15:32 +0100

    chore: code readibility improvement, finalizing project

--- Day 90 - Disappearing text web application/static/js/listener.js
+++ Day 90 - Disappearing text web application/static/js/listener.js
@@ -9,29 +9,30 @@ let boxShadowSize = 0;
 
 const timer = () => {
     elapsedTime += 100;
-    boxShadowSize += 0.4
+    boxShadowSize += 0.4;
 
     if (elapsedTime <= maxTime) {
         setTimeout(timer, step);
         console.log(elapsedTime);
         if (elapsedTime > 4000) {
             boxShadowSize += 0.4
-            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 6px crimson`
+            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 6px crimson`;
         } else if (elapsedTime > 3000) {
             boxShadowSize += 0.2
-            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 4px orange`
+            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 4px orange`;
         } else if (elapsedTime > 2000) {
-            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 2px yellow`
-        }
+            textArea.style.boxShadow = `0 0 ${boxShadowSize}px 2px yellow`;
+        };
     } else {
-        textArea.value = ''
-        textArea.style.boxShadow = 'none'
-        timerRunning = false
+        textArea.value = '';
+        textArea.style.boxShadow = 'none';
+        timerRunning = false;
     };
 };
 
 textArea.addEventListener('input', () => {
     elapsedTime = 0;
+    boxShadowSize = 0;
     if (!timerRunning) {
         timer();
         timerRunning = true;
