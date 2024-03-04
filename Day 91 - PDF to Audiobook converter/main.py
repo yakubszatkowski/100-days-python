b236b277dce09a0f79daec94461877934b591e05
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-03-04 05:55:39 +0100

    initializing project

---
+++ Day 91 - PDF to Audiobook converter/main.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+#TODO
+    # create gui that will be able to select .pdf files via directory 
+    # convert pdf text to text variable in python
+    # convert text to audio and save it as .mp3
