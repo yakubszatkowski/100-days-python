2e0446861c4ef31e92aa04f4b59ff456842e8375
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-03-24 04:53:22 +0100

    feat: basic website structure and styling

--- Day 92 - Image Colour Palette Generator/main.py
+++ Day 92 - Image Colour Palette Generator/main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        args = request.form
+        print(args)
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#TODO
+    # animated background - https://www.youtube.com/watch?v=E4qHOj1T1-w
+    # styling - title card in the middle
+    # post image form after choosing file
+    # transition after posting an image - title card goes on top, an analysis card on bottom
+    # analysis card - picture on the left, top 10 colors on the right with the HEX code inside of them - check d76
 
 # reference: https://www.coolphptools.com/color_extract#demo
