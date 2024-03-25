6b67aa3ee6001c5d98e25a44734ede47d663890e
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-03-25 08:11:40 +0100

    feat: upload image, save it in static folder

--- Day 92 - Image Colour Palette Generator/main.py
+++ Day 92 - Image Colour Palette Generator/main.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+import os
+
+UPLOAD_FOLDER = "./static/uploaded_images"
 
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        picture = request.form.get('picture-upload')
-        print(picture)
+        image = request.files['picture-upload']
+        image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+        filename = os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+        pil_image = Image.open(filename)
+        print(type(pil_image))
+
+        return render_template('index.html')
+
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
+
 #TODO
-    # post image form after choosing file
+    # get picture from user (POST method) and upload it
+    # delete the picture later
+    # analysis - https://colab.research.google.com/drive/1loupRmuDJGitzdvMcTB7o7Etim_-bjdQ#scrollTo=Fg75QSPht7bK
     # transition after posting an image - title card goes on top, an analysis card on bottom
     # analysis card - picture on the left, top 10 colors on the right with the HEX code inside of them - check d76
 
