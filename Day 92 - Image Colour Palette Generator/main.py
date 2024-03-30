87538c19894f986ef944978bbab9bb26bb02a50e
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-03-30 14:31:30 +0100

    feat: delete uploaded image via thread

--- Day 92 - Image Colour Palette Generator/main.py
+++ Day 92 - Image Colour Palette Generator/main.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, after_this_request
 from PIL import Image
+from threading import Thread
 from image_processing import dominant_colors_extraction
-import os
+import os, time
 
-UPLOAD_FOLDER = "./static/.cache"
+UPLOAD_FOLDER = "./static/temp_file"
 
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = UPLOAD_FOLDER
 
+def delete_file(path):
+    time.sleep(30)
+    os.remove(path)
+    return
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,7 +24,9 @@ def index():
         dominant_colors = dominant_colors_extraction(pil_image)
         filename = filename.replace(' ', '%20')
 
-        print(dominant_colors)
+        # workaround with threading because after_this_request decorator doesn't work on windows
+        thread = Thread(target=delete_file, args=(filename,)) 
+        thread.start()
 
         return render_template('index.html', colors_list=dominant_colors, image=filename)
     return render_template('index.html')
