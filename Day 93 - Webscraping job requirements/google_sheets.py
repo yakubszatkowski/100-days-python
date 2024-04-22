464065686f3eb6bd01464ce3219e3a9f07f4d9fd
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-04-22 08:39:46 +0200

    refactor: performance improvement; fix: StaleElementReferenceException issue handling

--- Day 93 - Webscraping job requirements/google_sheets.py
+++ Day 93 - Webscraping job requirements/google_sheets.py
@@ -7,6 +7,9 @@ client = gspread.authorize(creds)
 sheet_id = os.environ.get('google_sheets_id')
 sheet = client.open_by_key(sheet_id)
 
+# jt = 'Data Engineer'
+# tc= [('python', 38), ('sql', 33), ('etl', 32), ('azure', 31), ('aws', 28), ('spark', 24), ('apache', 16), ('agile', 14), ('git', 14), ('docker', 14), ('hadoop', 14), ('sap', 12), ('scala', 8), ('api', 8), ('snowflake', 8), ('java', 8), ('terraform', 7), ('pandas', 6), ('oracle', 6), ('dwh', 6), ('kafka', 6), 
+# ('scrum', 5), ('gcp', 5), ('looker', 5), ('kanban', 4), ('mongodb', 4), ('github', 4), ('saas', 4), ('paas', 4), ('kubernetes', 3), ('frontend', 2), ('go', 2), ('llm', 2), ('excel', 2), ('restful', 2), ('jenkins', 2), ('typescript', 2), ('helm', 2), ('gitlab', 2), ('tableau', 2), ('elasticsearch', 1), ('bigquery', 1)]
 
 def update_worksheet(job_title, technologies_count):
     todays_date = datetime.date.today().strftime('%d-%m-%Y')
@@ -31,16 +34,18 @@ def update_worksheet(job_title, technologies_count):
         odd_value_index = 0
         if odd_values:
             while set_worksheet_technologies != set_technologies:
-                worksheet.update_cell(row=1, col=first_empty_col_in_first_row, value=odd_values[odd_value_index])
+                try:
+                    worksheet.update_cell(row=1, col=first_empty_col_in_first_row, value=odd_values[odd_value_index])
+                except IndexError:
+                    break
                 first_empty_col_in_first_row += 1
                 odd_value_index += 1
-                set_worksheet_technologies = set(worksheet.row_values(1)[1:])
-    time.sleep(20)
+                # set_worksheet_technologies = set(worksheet.row_values(1)[1:])
+    time.sleep(25)
     
-
     # Insert datetime 
     first_column = worksheet.col_values(1)
-    if todays_date not in first_row:
+    if todays_date not in first_column:
         first_empty_row_in_first_col = len(first_column) + 1
         worksheet.update_cell(row=first_empty_row_in_first_col, col=1, value=todays_date)
 
@@ -53,3 +58,4 @@ def update_worksheet(job_title, technologies_count):
                 if technology.lower() == technology_name:
                     worksheet.update_cell(first_empty_row_in_first_col, technology_cell_col, count)
 
+# update_worksheet(jt, tc)
--- Day 93 - Webscraping job requirements/main.py
+++ Day 93 - Webscraping job requirements/main.py
@@ -16,92 +16,109 @@ password = os.environ.get('D32_gmail_pass')
 chrome_driver_path = os.environ.get('D48_chrome_driver_path')
 
 
-
 def find_job_titles():
     return job_list.find_elements(By.CSS_SELECTOR, 'li.jobs-search-results__list-item div.job-card-container div.artdeco-entity-lockup__content div.artdeco-entity-lockup__title a strong')
 
 
 def load_job_listing():
+    time.sleep(2)
     is_full_list = False
     while not is_full_list:
         job_titles = find_job_titles()
         job_titles_amount = len(job_titles)
         driver.execute_script("arguments[0].scrollIntoView();", job_titles[-1])
-        time.sleep(1)
+        time.sleep(0.5)
         new_job_titles_amount = len(find_job_titles())
         if new_job_titles_amount == job_titles_amount:
             is_full_list = True
-
     return job_titles
 
+def login_credentials(alternative_login):
+    if not alternative_login:
+        mail_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#session_key')))
+        password_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#session_password')))
+        submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, r'div[data-test-id="hero__content"] form[data-id="sign-in-form"] button[data-id="sign-in-form__submit-btn"]')))
+        mail_input.send_keys(email)
+        password_input.send_keys(password)
+        submit_button.click()
+    else:
+        sign_in_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, r'a[data-test-id="home-hero-sign-in-cta"]')))
+        sign_in_button.click()
+        alt_mail_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input#username')))
+        alt_password_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input#password')))
+        alt_submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[data-litms-control-urn="login-submit"]')))
+        alt_mail_input.send_keys(email)
+        alt_password_input.send_keys(password)
+        alt_submit_button.click()
 
 # Initialize driver
 options = Options()
 # options.add_argument('-headless=new')
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service(executable_path=chrome_driver_path, log_path="NUL"))
-wait = WebDriverWait(driver, 10)
+
+exceptions_to_ignore = (NoSuchElementException, StaleElementReferenceException)
+wait = WebDriverWait(driver, 5, ignored_exceptions=exceptions_to_ignore)
+
 driver.get('https://linkedin.com')
 driver.maximize_window()
 time.sleep(5)
 
-
 # Login
-mail_input = driver.find_element(By.CSS_SELECTOR, '#session_key')
-password_input = driver.find_element(By.CSS_SELECTOR, '#session_password')
-submit_button = driver.find_element(By.CSS_SELECTOR, r'div[data-test-id="hero__content"] form[data-id="sign-in-form"] button[data-id="sign-in-form__submit-btn"]')
-mail_input.send_keys(email)
-password_input.send_keys(password)
-submit_button.click()
+try:
+    login_credentials(alternative_login=False)
+except TimeoutException:
+    login_credentials(alternative_login=True)
 
 analyzed_job_titles = [
     '"machine learning"', '"data science"', '"data engineer"', '"data analyst"', 
-    '"software engineer"', '"web developer"', '"devops engineer"', '"mobile app developer"', 
-    '"automation engineer"'
+    '"software engineer"', '"web developer"', '"devops engineer"', 
+    '"mobile app developer"', '"automation engineer"'
 ]
 
 for analyzed_job_title in analyzed_job_titles:
-    print(analyzed_job_title)
+    
     # Searching for analyzed job title
-    # time.sleep(30)
-    jobs = driver.find_element(By.XPATH, '//*[@id="global-nav"]/div/nav/ul/li[3]/a')
+    print(analyzed_job_title)
+    jobs = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav"]/div/nav/ul/li[3]/a')))
     jobs.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    search_bar = driver.find_element(By.XPATH, '//*[@id="global-nav-search"]/div/div[2]')
+    search_bar = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav-search"]/div/div[2]')))
     search_bar.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    search_bar_input = driver.find_element(By.CSS_SELECTOR, 'div.jobs-search-box__inner input.jobs-search-box__keyboard-text-input')
+    search_bar_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.jobs-search-box__inner input.jobs-search-box__keyboard-text-input')))
     search_bar_input.send_keys(analyzed_job_title)
     search_bar_input.send_keys(Keys.ENTER)
-    time.sleep(2)
 
-    experience_level_options = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'button[id="searchFilter_experience"]')))
+    experience_level_options = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[id="searchFilter_experience"]')))
     experience_level_options.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    internship_checkbox = driver.find_element(By.CSS_SELECTOR, 'label[for=experience-1]')
-    entry_checkbox = driver.find_element(By.CSS_SELECTOR, 'label[for=experience-2]')
+    internship_checkbox = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'label[for="experience-1"]')))
+    entry_checkbox = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'label[for="experience-2"]')))
     internship_checkbox.click()
     entry_checkbox.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    show_results_button = driver.find_element(By.CSS_SELECTOR, 'div[id="hoverable-outlet-experience-level-filter-value"] button[data-control-name="filter_show_results"]')
+
+    show_results_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[id="hoverable-outlet-experience-level-filter-value"] button[data-control-name="filter_show_results"]')))
     show_results_button.click()
-    time.sleep(2)
+    time.sleep(1)
 
     # Load all the job listings by scrolling down as much as possible
-    job_list = driver.find_element(By.CSS_SELECTOR, 'ul.scaffold-layout__list-container')
+    job_list = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.scaffold-layout__list-container')))
     job_titles = load_job_listing()
+    print(len(job_titles))
     driver.execute_script("arguments[0].scrollIntoView();", job_titles[0])
 
     # Scrape job requirements
     job_requirements = ''
     for job_title in job_titles:
         job_title.click()
-        job_description = driver.find_element(By.CSS_SELECTOR, 'div#job-details div.mt4')
+        time.sleep(0.333)
+        job_description = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div#job-details div.mt4')))
         job_description_elements = job_description.find_elements(By.CSS_SELECTOR, 'span')
 
         for element in job_description_elements:
@@ -115,8 +132,6 @@ for analyzed_job_title in analyzed_job_titles:
                 line = html_object_text_only + '\n'
                 job_requirements += line 
 
-            
-    time.sleep(2)
     # Filtering and counting technology keywords
     most_common_words = keyword_count(job_requirements)
     print(most_common_words)
@@ -125,8 +140,7 @@ for analyzed_job_title in analyzed_job_titles:
 
 
 #TODO
-    # job search interface changed when 2nd loop of job search starts
-    # what to do if selenium object can't be found by the selector, how to repeat searching for selector? - WebDriverWait.until()
-    # figure a way to schedule this script once a day on any cloud service
+    # Quota exceeded for gspread write requests issue
+    # figure a way to schedule this script once a day on AWS Lambda
 
 print("--- %s seconds ---" % (time.time() - start_time))
