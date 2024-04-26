1e3da5620166a15cba405dfbc5dbf2581cf31768
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-04-26 13:23:21 +0200

    feat: start the game, jump and squat functions

--- Day 94 - Dino game automation/main.py
+++ Day 94 - Dino game automation/main.py
@@ -1,20 +1,48 @@
-import os
+import os, time
+import win32gui, win32con, win32api
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
-chrome_driver_path = os.environ.get('D48_chrome_driver_path')
+
+def jump():
+    win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_UP, 0)
+    win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_UP, 0)
+
+
+def squat():
+    win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_DOWN, 0)
+    time.sleep(0.333)
+    win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_DOWN, 0)
 
 
 def main_script():
-    # Initialize driver
-    options = Options()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options, service=Service(executable_path=chrome_driver_path, log_path="NUL"))
-    driver.get('https://elgoog.im/dinosaur-game/')
-    driver.maximize_window()
+    # Entering the game
+    privacy_popout_window_agree_button = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'button.css-47sehv')))
+    privacy_popout_window_agree_button.click()
+    jump()
+    game_on = True
+    time.sleep(3)
+    squat()
 
 
 if __name__ == '__main__':
+    # Initialize the driver
+    WINDOW_NAME = 'Play Chrome Dinosaur Game Online - elgooG - Google Chrome'
+    chrome_driver_path = os.environ.get('D48_chrome_driver_path')
+    options = Options()
+    options.add_experimental_option("detach", True)  # for dev
+    driver = webdriver.Chrome(options=options, service=Service(executable_path=chrome_driver_path, log_path="NUL"))
+    driver.get('https://elgoog.im/dinosaur-game/')
+    driver.maximize_window()
+    wait = WebDriverWait(driver, 5)
+    hwnd = win32gui.FindWindow(None, WINDOW_NAME)
+
+    # Initialize main script
     main_script()
+    
