b4a049802c408203a8bff43e3f7c035d3da46a04
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-05-01 00:13:52 +0200

    feat: enemy object detection

--- Day 94 - Dino game automation/main.py
+++ Day 94 - Dino game automation/main.py
@@ -11,8 +11,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from screen_capture import get_game_screen
 from game_object import GameObject
 from utils import find_img
-import numpy as np
-from PIL import Image
+
 
 def jump():
     actions.send_keys(Keys.UP).perform()
@@ -30,22 +29,38 @@ def main_script():
     time.sleep(1)
     privacy_popout_window_agree_button = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'button.css-47sehv')))
     privacy_popout_window_agree_button.click()
-    
     jump()
     game_on = True
+    day_index = 0
     
     # Game loop
     while game_on:
+
         # Update game screen
         game_screen = get_game_screen(hwnd, region)
 
-        if player.match(game_screen):
-            cv2.rectangle(game_screen, player.location[0], player.location[1], (0,0,255), 2)
+        # Establish whenever it's day or night; dino/player detection
+        if player[0].match(game_screen):
+            day_index = 0
+            cv2.rectangle(game_screen, player[day_index].location[0], player[day_index].location[1], (0,0,255), 2)
+        elif player[1].match(game_screen):
+            day_index = 1
+            cv2.rectangle(game_screen, player[day_index].location[0], player[day_index].location[1], (0,0,255), 2)
+        
+        # Detection of cacti and birds
+        for dodge_object in dodge_objects[day_index]:
+            if dodge_object.match(game_screen):
+                cv2.rectangle(game_screen, dodge_object.location[0], dodge_object.location[1], (0,0,255), 2)
+
 
 
         cv2.imshow('screen', game_screen)
         if cv2.waitKey(1) == ord('q'):
             break
+
+# ((x1, y1), (x2, y2))
+# ((9, 296), (137, 389))
+# ((9, 94), (137, 187))
 
 
 if __name__ == '__main__':
@@ -61,19 +76,14 @@ if __name__ == '__main__':
     wait = WebDriverWait(driver, 10)
     hwnd = win32gui.FindWindow(None, WINDOW_NAME)
     actions = ActionChains(driver)
-    region = (370, 820 , 60, 1000)
+    region = (370, 820 , 60, 1600)
 
-    player = GameObject(find_img('dino_day.png'))
-
-    # player = [GameObject(find_img('dino_day.png')), GameObject(find_img('dino_night.png'))]
-    # dodge_objects = [
-    #     [GameObject(find_img('big_cacti_night.png')), GameObject(find_img('small_cacti_night.png')), GameObject(find_img('bird_night.png'))],
-    #     [GameObject(find_img('big_cacti_day.png')), GameObject(find_img('small_cacti_day.png')), GameObject(find_img('bird_day.png'))]
-    # ]
-    
+    # Initialize game objects
+    player = [GameObject(find_img('dino_day.png')), GameObject(find_img('dino_night.png'))]
+    dodge_objects = [
+        [GameObject(find_img('big_cacti_day.png')), GameObject(find_img('small_cacti_day.png')), GameObject(find_img('bird_day.png'))],
+        [GameObject(find_img('big_cacti_night.png')), GameObject(find_img('small_cacti_night.png')), GameObject(find_img('bird_night.png'))]
+    ]
 
     # Initialize main script
     main_script()
-
-#TODO
-    # detect object within picture frame - https://www.youtube.com/watch?v=mIojvoMRerU
