7293201130a897d4277e06e3697e24f2b6d01a9a
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-08-19 02:05:32 +0200

    feat: star rating

--- Day 96 - Random Cat Facts Website/cat_facts_app/urls.py
+++ Day 96 - Random Cat Facts Website/cat_facts_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('start/', views.start, name='start'),
-    path('random_cat/', views.random_cat_picture, name='random_cat_picture')
+    path('random_cat/', views.random_cat_picture, name='random_cat_picture'),
+    path('random_cat_breed/', views.random_cat_picture_by_breed, name='random_cat_picture_by_breed')
 ]
