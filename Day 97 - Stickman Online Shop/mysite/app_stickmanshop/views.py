20ae315df333b46df0ab214f1ce8d18e81c12872
yakubszatkowski <yakub.szatkowski@gmail.com>
2024-11-03 12:26:43 +0100

    refactor: render base stickman image from base64 data

--- Day 97 - Stickman Online Shop/mysite/app_stickmanshop/views.py
+++ Day 97 - Stickman Online Shop/mysite/app_stickmanshop/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from PIL import Image
+from io import StringIO, BytesIO
+
+import os, base64
 
 # Create your views here.
 def home(request):
@@ -7,8 +12,16 @@ def home(request):
 
 
 def create(request):
+    path_static_img = os.path.join(settings.BASE_DIR, 'app_stickmanshop', 'static', 'img')
 
-    return render(request, 'create.html', {})
+    # Opening base stickman image and saving it to the buffer
+    base_img_stickman = Image.open(f'{path_static_img}/base_stickman.jpg').convert('RGB')
+    buffer_stickman = BytesIO()
+    base_img_stickman.save(buffer_stickman, 'JPEG')
+    buffer_stickman.seek(0)
+    stickman_image_base64 = base64.b64encode(buffer_stickman.getvalue()).decode('utf-8')
+
+    return render(request, 'create.html', {'stickman_image': stickman_image_base64})
 
 
 def collection(request):
