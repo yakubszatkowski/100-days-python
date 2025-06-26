a3f01caf28e7c0228604bc3310559a15fb9b08d6
yakubszatkowski <yakub.szatkowski@gmail.com>
2025-06-26 19:24:18 +0200

    feat: connect python backend with electronjs gui

--- Day 98 - Anki Flashcard maker with ChatGPT API/gui/main.js
+++ Day 98 - Anki Flashcard maker with ChatGPT API/gui/main.js
@@ -1,22 +1,38 @@
 const { app, BrowserWindow } = require('electron')
+const {PythonShell} = require('python-shell')
+const path = require('path')
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 600,
-      height: 800
-    })
-  
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,  // for Node in renderer
+            contextIsolation: false
+        }
+    })  
     win.loadFile('templates/index.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+    var options = {
+        scriptPath : path.join(__dirname),
+        args : [],
+    };
+
+    let pyshell = new PythonShell('test2.py', options);
+
+    pyshell.on('message', function(message) {
+        console.log(message);
+        console.log(typeof message);
+    });
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
+// npm run start
