23c77c3513fdc51684d7a8557c326a292753db33
yakubszatkowski <yakub.szatkowski@gmail.com>
2025-09-15 09:11:44 +0200

    output to csv formatting

--- Day 98 - Anki Flashcard maker with ChatGPT API/engine/d_csv_format.py
+++ Day 98 - Anki Flashcard maker with ChatGPT API/engine/d_csv_format.py
@@ -1,3 +1,9 @@
+import pandas as pd
 from test import output
 
-print(output)
+
+def save_to_csv(save_path):
+    cards = "\n\n".join(output).split("\n\n")
+    flashcard_df = (pd.DataFrame([c.split(";", 1) for c in cards], columns=["front", "note"]))
+    flashcard_df.to_csv(save_path)
+
